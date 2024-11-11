c6559bed0cc600191bc2866da6db952fed8e256d
Long Pham <longpd1911@gmail.com>
2024-11-12 01:55:35 +0700

    chu nig

--- src/main/java/com/app/chuushajou/controllers/CustomerCtrl.java
+++ src/main/java/com/app/chuushajou/controllers/CustomerCtrl.java
@@ -10,6 +10,7 @@ import com.app.chuushajou.services.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -43,7 +44,57 @@ public class CustomerCtrl {
                             "data", customerDTO)
             );
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ResMap.of("status", "error", "message", e.getMessage())
+            );
+        }
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> addCustomer(@RequestBody CustomerDTO customerDTO) {
+        try {
+            CustomerDTO newCustomerDTO = customerService.addCustomer(customerDTO);
+            return ResponseEntity.ok(
+                    ResMap.of(
+                            "status", "success",
+                            "data", newCustomerDTO)
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ResMap.of("status", "error", "message", e.getMessage())
+            );
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateCustomer(@RequestBody CustomerDTO customerDTO, @PathVariable("id") Long customerId) {
+        try {
+            CustomerDTO updatedCustomerDTO = customerService.updateCustomer(customerDTO, customerId);
+            return ResponseEntity.ok(
+                    ResMap.of(
+                            "status", "success",
+                            "data", updatedCustomerDTO)
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ResMap.of("status", "error", "message", e.getMessage())
+            );
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> removeCustomer(@PathVariable("id") Long customerId) {
+        try {
+            customerService.removeCustomer(customerId);
+            return ResponseEntity.ok(
+                    ResMap.of(
+                            "status", "success",
+                            "message", "Customer removed successfully")
+            );
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                    ResMap.of("status", "error", "message", e.getMessage())
+            );
         }
     }
 }
---
+++ src/main/java/com/app/chuushajou/controllers/VehicleTypeCtrl.java
@@ -0,0 +1,5 @@
+package com.app.chuushajou.controllers;
+
+public class VehicleTypeCtrl {
+
+}
